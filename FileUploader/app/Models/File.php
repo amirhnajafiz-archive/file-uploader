98bd2c26d3c770aac0781f6428291818c978adcf
9831065 <najafizadeh21@gmail.com>
2021-09-11 11:59:35 +0430

    Added File model

---
+++ FileUploader/app/Models/File.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class File extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'img',
+        'link',
+        'user_id'
+    ];
+}
